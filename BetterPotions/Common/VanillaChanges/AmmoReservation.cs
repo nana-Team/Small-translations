26534ea0de2823013f1d5f63c23281e333b8dd42
Teftelnik <timoshahero@gmail.com>
2025-04-12 23:22:07 +0300

    Add files via upload

---
+++ BetterPotions/Common/VanillaChanges/AmmoReservation.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using BetterPotions.Common.Configs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace BetterPotions.Common.VanillaChanges;
+
+public class AmmoReservationGlobalBuff : GlobalBuff
+{
+    public override bool IsLoadingEnabled(Mod mod) => BetterPotionsConfig.Instance.PotionChanges_AmmoReservation;
+
+    public override void Update(int type, Player player, ref int buffIndex) {
+        if (type != BuffID.AmmoReservation) {
+            return;
+        }
+
+        player.ammoPotion = false;
+        player.GetModPlayer<AmmoReservationPlayer>().AmmoReservation = true;
+    }
+
+    public override void ModifyBuffText(int type, ref string buffName, ref string tip, ref int rare) {
+        if (type != BuffID.AmmoReservation) {
+            return;
+        }
+
+        tip = Language.GetTextValue("Mods.BetterPotions.VanillaChanges.AmmoReservationTip");
+    }
+}
+
+public class AmmoReservationGlobalItem : GlobalItem
+{
+    public override bool IsLoadingEnabled(Mod mod) => BetterPotionsConfig.Instance.PotionChanges_AmmoReservation;
+
+    public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.AmmoReservationPotion;
+
+    public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+        TooltipLine line = tooltips.FirstOrDefault(tip => tip.Name == "Tooltip0");
+        if (line is null) {
+            return;
+        }
+
+        line.Text = Language.GetTextValue("Mods.BetterPotions.VanillaChanges.AmmoReservationBuffTip");
+    }
+}
+
+public class AmmoReservationPlayer : ModPlayer
+{
+    public bool AmmoReservation { get; set; }
+
+    public override void ResetEffects() {
+        AmmoReservation = false;
+    }
+
+    public override bool CanConsumeAmmo(Item weapon, Item ammo) {
+        if (AmmoReservation && Main.rand.NextFloat() < 0.5f) {
+            return false;
+        }
+
+        return base.CanConsumeAmmo(weapon, ammo);
+    }
+}
---
+++ BetterPotions/Common/VanillaChanges/ManaRegeneration.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Linq;
+using BetterPotions.Common.Configs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace BetterPotions.Common.VanillaChanges;
+
+public class ManaRegenerationGlobalBuff : GlobalBuff
+{
+    public override bool IsLoadingEnabled(Mod mod) => BetterPotionsConfig.Instance.PotionChanges_ManaRegen;
+
+    public override void Update(int type, Player player, ref int buffIndex) {
+        if (type != BuffID.ManaRegeneration) {
+            return;
+        }
+
+        player.GetModPlayer<ManaRegenerationPlayer>().ManaRegeneration = true;
+    }
+
+    public override void ModifyBuffText(int type, ref string buffName, ref string tip, ref int rare) {
+        if (type != BuffID.ManaRegeneration) {
+            return;
+        }
+
+        tip = Language.GetTextValue("Mods.BetterPotions.VanillaChanges.ManaRegenerationTip");
+    }
+}
+
+public class ManaRegenerationPlayer : ModPlayer
+{
+    private int manaRegenerationCounter;
+
+    public bool ManaRegeneration { get; set; }
+
+    public override void ResetEffects() {
+        ManaRegeneration = false;
+    }
+
+    public override void PostUpdateBuffs() {
+        if (!ManaRegeneration) {
+            return;
+        }
+
+        manaRegenerationCounter++;
+        if (manaRegenerationCounter > 15 && Player.statMana < Player.statManaMax2) {
+            Player.statMana++;
+            manaRegenerationCounter = 0;
+        }
+    }
+}
+
+public class ManaRegenerationGlobalItem : GlobalItem
+{
+    public override bool IsLoadingEnabled(Mod mod) => BetterPotionsConfig.Instance.PotionChanges_ManaRegen;
+
+    public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.ManaRegenerationPotion;
+
+    public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+        TooltipLine line = tooltips.FirstOrDefault(tip => tip.Name == "Tooltip0");
+        if (line is null) {
+            return;
+        }
+
+        line.Text = Language.GetTextValue("Mods.BetterPotions.VanillaChanges.ManaRegenerationBuffTip");
+    }
+}

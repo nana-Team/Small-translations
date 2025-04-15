9dfad6e891cc455cb0fa95eae7256c287f4285bb
Teftelnik <timoshahero@gmail.com>
2025-04-15 15:01:21 +0300

    Update ImprovedTooltips.cs

--- BetterPotions/Common/VanillaChanges/ImprovedTooltips.cs
+++ BetterPotions/Common/VanillaChanges/ImprovedTooltips.cs
@@ -19,24 +19,24 @@ public class ImprovedTooltipsGlobalItem : GlobalItem
         }
 
         line.Text = item.type switch {
-            ItemID.BattlePotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BattlePotion");
-            ItemID.CalmingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.CalmingPotion");
-            ItemID.CratePotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.CratePotion");
-            ItemID.TrapsightPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.TrapsightPotion");
-            ItemID.FeatherfallPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FeatherfallPotion");
-            ItemID.FishingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FishingPotion");
-            ItemID.FlipperPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FlipperPotion");
-            ItemID.GillsPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.GillsPotion");
-            ItemID.GravitationPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.GravitationPotion");
-            ItemID.InvisibilityPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.InvisibilityPotion");
-            ItemID.IronskinPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.IronskinPotion");
-            ItemID.MagicPowerPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.MagicPowerPotion");
-            ItemID.NightOwlPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.NightOwlPotion");
-            ItemID.RegenerationPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.RegenerationPotion");
-            ItemID.SwiftnessPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.SwiftnessPotion");
-            ItemID.TitanPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.TitanPotion");
-            ItemID.WarmthPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.WarmthPotion");
-            ItemID.WaterWalkingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.WaterWalkingPotion");
+            ItemID.BattlePotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BattlePotion"),
+            ItemID.CalmingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.CalmingPotion"),
+            ItemID.CratePotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.CratePotion"),
+            ItemID.TrapsightPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.TrapsightPotion"),
+            ItemID.FeatherfallPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FeatherfallPotion"),
+            ItemID.FishingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FishingPotion"),
+            ItemID.FlipperPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.FlipperPotion"),
+            ItemID.GillsPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.GillsPotion"),
+            ItemID.GravitationPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.GravitationPotion"),
+            ItemID.InvisibilityPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.InvisibilityPotion"),
+            ItemID.IronskinPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.IronskinPotion"),
+            ItemID.MagicPowerPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.MagicPowerPotion"),
+            ItemID.NightOwlPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.NightOwlPotion"),
+            ItemID.RegenerationPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.RegenerationPotion"),
+            ItemID.SwiftnessPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.SwiftnessPotion"),
+            ItemID.TitanPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.TitanPotion"),
+            ItemID.WarmthPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.WarmthPotion"),
+            ItemID.WaterWalkingPotion => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.WaterWalkingPotion"),
             _ => line.Text
         };
     }
@@ -48,22 +48,22 @@ public class ImprovedTooltipsGlobalBuff : GlobalBuff
 
     public override void ModifyBuffText(int type, ref string buffName, ref string tip, ref int rare) {
         tip = type switch {
-            BuffID.Battle => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffBattle");
-            BuffID.Calm => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffCalm");
-            BuffID.Crate => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffCrate");
-            BuffID.Dangersense => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffDangersense");
-            BuffID.Featherfall => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFeatherfall");
-            BuffID.Fishing => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFishing");
-            BuffID.Flipper => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFlipper");
-            BuffID.Gills => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffGills");
-            BuffID.Gravitation => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffGravitation");
-            BuffID.Invisibility => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffInvisibility");
-            BuffID.Ironskin => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffIronskin");
-            BuffID.NightOwl => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffNightOwl");
-            BuffID.Regeneration => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffRegeneration");
-            BuffID.Titan => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffTitan");
-            BuffID.Warmth => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffWarmth");
-            BuffID.WaterWalking => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffWaterWalking");
+            BuffID.Battle => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffBattle"),
+            BuffID.Calm => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffCalm"),
+            BuffID.Crate => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffCrate"),
+            BuffID.Dangersense => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffDangersense"),
+            BuffID.Featherfall => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFeatherfall"),
+            BuffID.Fishing => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFishing"),
+            BuffID.Flipper => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffFlipper"),
+            BuffID.Gills => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffGills"),
+            BuffID.Gravitation => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffGravitation"),
+            BuffID.Invisibility => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffInvisibility"),
+            BuffID.Ironskin => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffIronskin"),
+            BuffID.NightOwl => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffNightOwl"),
+            BuffID.Regeneration => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffRegeneration"),
+            BuffID.Titan => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffTitan"),
+            BuffID.Warmth => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffWarmth"),
+            BuffID.WaterWalking => Language.GetTextValue("Mods.BetterPotions.VanillaChanges.BuffWaterWalking"),
             _ => tip
         };
     }
